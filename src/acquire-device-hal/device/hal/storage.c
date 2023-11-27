9cb20d0c026d72a6c28654f5891560fb395160b4
Alan Liddell <aliddell@chanzuckerberg.com>
2023-11-27 16:21:01 -0500

    Define and implement storage_destroy

--- src/acquire-device-hal/device/hal/storage.c
+++ src/acquire-device-hal/device/hal/storage.c
@@ -176,6 +176,17 @@ Error:
 }
 
 enum DeviceStatusCode
+storage_destroy(struct Storage* self)
+{
+    CHECK(self);
+    CHECK(self->destroy);
+    self->destroy(self);
+    return Device_Ok;
+Error:
+    return Device_Err;
+}
+
+enum DeviceStatusCode
 storage_reserve_image_shape(struct Storage* self,
                             const struct ImageShape* shape)
 {
